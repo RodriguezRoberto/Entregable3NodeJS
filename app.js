54156b664d65ece64761e70d0b1daab78ee0e806
Roberto Rodriguez <rdrgzmndz.sm0101@gmail.com>
2022-05-16 16:22:14 -0500

    1st commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require( 'express' );
+const cors = require('cors');
+
+const { globalErrorHandler } = require('./controllers/errors.controller');
+
+// Routers
+const { usersRouter } = require( './routes/users.routes' );
+const { repairsRouter } = require( './routes/repairs.routes' );
+
+// Init express app
+const app = express();
+
+// Enable CORS
+app.use(cors());
+
+// Enable incoming JSON data
+app.use(express.json());
+
+app.use( '/api/v1/users', usersRouter );
+app.use( '/api/v1/repairs', repairsRouter );
+
+//Global error handler
+app.use('*', globalErrorHandler);
+
+module.exports = { app };
