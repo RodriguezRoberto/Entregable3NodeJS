45545c121b1e5008fc63f0af12c2e401be5e237b
Roberto Rodriguez <rdrgzmndz.sm0101@gmail.com>
2022-05-25 19:06:28 -0500

    6th commit

--- utils/database.js
+++ utils/database.js
@@ -11,6 +11,16 @@ const db = new Sequelize({
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
     logging: false,
+    dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+
   });
 
 module.exports = { db };
